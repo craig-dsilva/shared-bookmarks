21e26c45e0234640ec915a1a578afb252ecb6fa0
Seddiq Azam <sedazam@gmail.com>
2026-02-12 17:01:23 +0000

    Fix formatting and spacing in populateUserDropdown function for improved readability

--- script.js
+++ script.js
@@ -13,16 +13,16 @@ window.onload = function () {
 };
 
 function populateUserDropdown() {
-  const users= getUserIds();
+  const users = getUserIds();
   const dropdown = document.getElementById("user-select");
 
-users.forEach((userId) => {
-const option = document.createElement("option");
-option.value = userId;
-option.text = `User ${userId}`;
-dropdown.appendChild(option);
-});
-}
+  users.forEach((userId) => {
+    const option = document.createElement("option");
+    option.value = userId;
+    option.text = `User ${userId}`;
+    dropdown.appendChild(option);
+  });
+
   const users = getUserIds();
   document.querySelector("body").innerText = `There are ${users.length} users`;
-};
+}
